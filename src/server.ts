ad5d2a6d95b93768b644b9aa31f68f0ce1ce14c8
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-27 17:15:36 -0300

    feat: implementado o server

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import CustomRouter from './routes/Router';
+import App from './app';
+import CarController from './controllers/Car';
+import { Car } from './interfaces/CarInterface';
+
+const server = new App();
+
+const carController = new CarController();
+
+const carRouter = new CustomRouter<Car>();
+carRouter.addRoute(carController);
+
+server.addRouter(carRouter.router);
+
+export default server;
